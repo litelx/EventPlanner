66ffeff00e4deceb8868954a8dfd965664bbc809
miacaplan <miacaplan@gmail.com>
2016-06-02 14:58:45 +0300

    added Guest and remove many2many for now

--- events/models.py
+++ events/models.py
@@ -36,44 +36,71 @@ class Item(models.Model):
 
 class Slot(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='slots')
-    guest = models.ForeignKey(auth.models.User, related_name='slots', null=True, blank=True) # TODO: what's the best way to contrain to guests + host
+    guest = models.ForeignKey(auth.models.User, related_name='slots', null=True,
+                              blank=True)  # TODO: what's the best way to contrain to guests + host
     comment = models.CharField(max_length=100, null=True, blank=True)
     index = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return "{} #{}".format(self.item.title, self.index)
 
-class EventGuests(models.Model):
-    ''' many2many relationship events - guests
-    '''
-    guest = models.ForeignKey(auth.models.User, related_name='guest_events')
-    event = models.ForeignKey(Event, related_name='event_guests')
-    can_edit = models.BooleanField(default=False)
 
-    class Meta:
-        unique_together = (('guest', 'event'))
+class Guest(models.Model):
+    class Status:
+        NO_RESPONSE = 1
+        ACCEPTED = 2
+        DECLINED = 3
+        MAYBE = 4
+
+        choices = ((NO_RESPONSE, 'no response'), (ACCEPTED, 'accepted'), (DECLINED, 'declined'), (MAYBE, 'maybe'))
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    status = models.IntegerField(choices=Status.choices, default=Status.NO_RESPONSE)
+    invited_by = models.ForeignKey(auth.models.User, related_name='invitees')
+    invited_at = models.DateTimeField()
+    secret_code = models.CharField(max_length=200, unique=True)
+    user = models.ForeignKey(auth.models.User, null=True, blank=True, related_name='invitations')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.guest.username
+        return "{}: {} {}".format(self.event.title, self.name, self.email)
 
-            # class Person(models.Model):
-#         first_name = models.CharField(max_length=30)
-#         last_name = models.CharField(max_length=30)
-#         birthday = models.DateTimeField()
-#
-#         def __str__(self):
-#                 return self.last_name + ",  " + self.first_name
+    class Meta:
+        unique_together = (('event', 'email'))
 
-# class Ad(models.Model):
-#     posted_at = models.DateTimeField(auto_now_add=True)
-#
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#
-#     posted_by = models.CharField(max_length=200)
-#     contact_email = models.EmailField()
-#     contact_phone = models.CharField(max_length=30,  null=True, blank=True)
-#     price = models.PositiveIntegerField(null=True, blank=True)
+
+    # class EventGuests(models.Model):
+    #     ''' many2many relationship events - guests
+    #     '''
+    #     guest = models.ForeignKey(auth.models.User, related_name='guest_events')
+    #     event = models.ForeignKey(Event, related_name='event_guests')
+    #     can_edit = models.BooleanField(default=False)
+    #
+    #     class Meta:
+    #         unique_together = (('guest', 'event'))
+    #
+    #     def __str__(self):
+    #         return self.guest.username
+
+    # class Person(models.Model):
+    #         first_name = models.CharField(max_length=30)
+    #         last_name = models.CharField(max_length=30)
+    #         birthday = models.DateTimeField()
+    #
+    #         def __str__(self):
+    #                 return self.last_name + ",  " + self.first_name
+
+    # class Ad(models.Model):
+    #     posted_at = models.DateTimeField(auto_now_add=True)
+    #
+    #     title = models.CharField(max_length=200)
+    #     description = models.TextField()
+    #
+    #     posted_by = models.CharField(max_length=200)
+    #     contact_email = models.EmailField()
+    #     contact_phone = models.CharField(max_length=30,  null=True, blank=True)
+    #     price = models.PositiveIntegerField(null=True, blank=True)
     # price = models.DateTimeField()
 
     # o = Ad()
